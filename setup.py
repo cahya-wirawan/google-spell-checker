539c11e205fc044da4f9f9d2b10b05e7e1a5d79a
Cahya Wirawan <cahya.wirawan@gmail.com>
2023-01-27 12:54:40 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setup(
     name='google_spell_checker',
-    version='0.2.0',
+    version='0.2.1',
     description='Google Spell Checker',
     long_description=long_description,
     long_description_content_type="text/markdown",
